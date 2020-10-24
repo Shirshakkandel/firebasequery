23519cb634d4983ea12172122e0a44c2032dac3e
ShirshakKandel <shirshakkandel@gmail.com>
2020-10-24 15:45:37 +0545

    1)Part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import firebase from './firebase'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ref = firebase.firestore().collection('schools')
+  const [schools, setSchools] = useState([]);
+  const [loading, setloading] = useState(false)
+
+  //using map 
+  // useEffect(() => {
+  //   setloading(true);
+  //   ref.onSnapshot(snapshot => {
+  //     const items = [];
+  //     snapshot.docs.map(doc => {
+  //       items.push(doc.data());
+  //     });
+
+  //     setSchools(items);
+  //     setloading(false);
+  //   })
+  // })
+
+  // real time database using onSnapshot
+  // function getSchools() {
+  //   setloading(true);
+  //   ref.onSnapshot(querySnapshot => {
+  //     const items = [];
+  //     querySnapshot.forEach(doc => {
+  //       items.push(doc.data());
+  //     })
+  //     setSchools(items);
+  //     setloading(false);
+  //   })
+  // }
+
+
+  //one time get method
+
+  useEffect(() => {
+    setloading(true);
+    ref.onSnapshot(snapshot => {
+      setSchools(snapshot.docs.map(doc => doc.data()))
+    })
+    setloading(false);
+  }, [])
+
+  // function getSchools() {
+  //   setloading(true);
+  //   ref.get().then(item => {
+  //     const items = item.docs.map(doc => doc.data())
+  //     setSchools(items);
+  //     setloading(false)
+  //   })
+  // }
+
+  // useEffect(() => {
+  //   getSchools();
+  // }, [])
+
+  // useEffect(() => {
+  //   setloading(true)
+  //   ref.get().then(item => {
+  //     setSchools(item.docs.map(doc => doc.data()))
+  //   })
+  //   setloading(false)
+  // }, [])
+
+
+  if (loading) {
+    return <h1>Loading</h1>
+  }
+
+    return (
+      <div className="App">
+        <h1>Schools</h1>
+        {schools.map(school => (
+          <div key={school.id}>
+            <h2>{school.title}</h2>
+            <p>{school.desc}</p>
+          </div>
+        ))}
+
+      </div>
+  )
+
 }
 
-export default App;
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyCBvizLF9fY7c3c8DxglWdIDBwYt3qVw1M",
+    authDomain: "school-directory-72117.firebaseapp.com",
+    databaseURL: "https://school-directory-72117.firebaseio.com",
+    projectId: "school-directory-72117",
+    storageBucket: "school-directory-72117.appspot.com",
+    messagingSenderId: "509970680403",
+    appId: "1:509970680403:web:37e61e1b715d42b57994a6"
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;
+
